85f984c0b073e2a386d4a809f3b2391b433b40fb
Luca Picci <lpicci96@gmail.com>
2023-02-09 15:20:02 +0100

    create raw data script

--- scripts/extraction_tools/hdi_indices.py
+++ scripts/extraction_tools/hdi_indices.py
@@ -1,1 +1,20 @@
 """Extract and format HDI indices"""
+
+from bblocks.import_tools import hdr
+
+
+def download_raw_hdi():
+    hdr_data = hdr.HDR().load_data(hdr.available_indicators('gii')
+                                   + hdr.available_indicators('gdi')
+                                   )
+
+    # save gii
+    (hdr_data
+     .get_data(hdr.available_indicators('gii'))
+     .to_csv('data/raw/hdi/hdr_gii.csv', index=False))
+
+    # save gdi
+    (hdr_data
+     .get_data(hdr.available_indicators('gdi'))
+     .to_csv('data/raw/hdi/hdr_gdi.csv', index=False))
+
