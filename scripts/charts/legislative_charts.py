50211c6efa8117c3715c36ce3a14c562ac138a73
Luca Picci <lpicci96@gmail.com>
2023-03-01 17:53:01 +0100

    create law charts

--- scripts/charts/legislative_charts.py
+++ scripts/charts/legislative_charts.py
@@ -1,8 +1,7 @@
 """Charts relating to laws and political participation"""
 
 import pandas as pd
-import numpy as np
-import country_converter as coco
+from scripts.logger import logger
 
 from scripts.config import PATHS
 from scripts import common
@@ -14,44 +13,55 @@ employment_laws = ['SG.GET.JOBS.EQ', "SG.BUS.REGT.EQ",
                    "SG.LEG.SXHR.EM",
                    "SG.PEN.SXHR.EM"]
 
-
-def get_law_data() -> pd.DataFrame:
-    """Get law data"""
-    value_dict = {0: -1, 1: 1}
-    name_dict = {-1: 'no', 1: 'yes'}
-
-    return (LAWS
-            .loc[lambda d: d.indicator_code.isin(employment_laws)]  # only keep employment laws
-            .assign(year=lambda d: pd.to_datetime(d['date']).dt.year,
-                    value=lambda d: d.value.map(value_dict),
-                    name=lambda d: d.value.map(name_dict)
-                    )
-            .pipe(common.latest_value, 'value', ['iso_code', 'indicator_code'], 'year')
-            .pivot(index=['indicator_name', 'entity_name'], columns='name', values='value')
-            .reset_index()
-            )
+laws_dict = {'entrepreneurship_laws': ['SG.LAW.CRDD.GR', 'SG.CNT.SIGN.EQ', 'SG.BUS.REGT.EQ',
+                                       'SG.OPN.BANK.EQ'],
+             'workplace_laws': ['SG.GET.JOBS.EQ', "SG.LAW.NODC.HR", "SG.LEG.SXHR.EM",
+                                "SG.PEN.SXHR.EM"],
+             'pay_laws': ['SG.LAW.EQRM.WK', 'SG.NGT.WORK.EQ', 'SG.DNG.WORK.DN.EQ',
+                          'SG.IND.WORK.EQ']}
 
 
-def chart_marimekko() -> pd.DataFrame:
-    """ """
+def make_marimekko(indicators: list) -> pd.DataFrame:
+    """Create a marimekko chart for the given indicators
+
+    Args:
+        indicators: list of indicator codes to include in the chart
+    """
 
     value_dict = {0: -1, 1: 1}
     name_dict = {-1: 'no', 1: 'yes'}
 
     df = (LAWS
-            .loc[lambda d: d.indicator_code.isin(employment_laws)]  # only keep employment laws
-            .assign(year=lambda d: pd.to_datetime(d['date']).dt.year,
-                    value=lambda d: d.value.map(value_dict),
-                    name=lambda d: d.value.map(name_dict)
-                    )
-            .pipe(common.latest_value, 'value', ['iso_code', 'indicator_code'], 'year')
-            .assign(female_pop = lambda d: d.iso_code.map(common.female_population()))
-            )
+          .loc[lambda d: d.indicator_code.isin(indicators)]
+          .dropna(subset='value')
+          .assign(year=lambda d: pd.to_datetime(d['date']).dt.year,
+                  value=lambda d: d.value.map(value_dict),
+                  value_label=lambda d: d.value.map(name_dict))
+          .pipe(common.latest_value, 'value', ['iso_code', 'indicator_code'], 'year')
+          .assign(female_pop=lambda d: d.iso_code.map(common.female_population()))
+          .loc[:, ['indicator_name', 'entity_name', 'year', 'value_label', 'value', 'female_pop']]
+          )
 
-    total_population = df.groupby('indicator_code')['female_pop'].sum().unique()[0]
-    df = (df.assign(female_pop_pct = lambda d: (d.female_pop / total_population) * 100))
+    # create bar width
+    total_population = df.groupby('indicator_name')['female_pop'].sum().unique()[0]
+    df = (df.assign(width=lambda d: (d.female_pop / total_population) * 100))
+    df.loc[df.width < 1, 'width'] = 1  # make sure all bars are visible
 
-    df.loc[df.female_pop_pct < 1, 'female_pop_pct'] = 1
+    # convert raw population values to millions, sort by population
+    df = (df.sort_values(by = 'female_pop', ascending=False)
+          .assign(female_pop_annot=lambda d: (d.female_pop / 1000000))
+          )
 
     return df
 
+
+def chart_laws_marimekko() -> None:
+    """Create a marimekko chart of the laws used in the WBL index
+    and save to csv
+    """
+
+    for law_type, indicators in laws_dict.items():
+        (make_marimekko(indicators)
+         .to_csv(f'{PATHS.output}/laws_{law_type}.csv', index=False)
+         )
+        logger.info(f'Created {law_type} marimekko chart')
